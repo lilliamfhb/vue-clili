a6b2ef31f5759cac6573a1e4faa8385e0c2cc589
Lilliam <lilliam.harobernal@gmail.com>
2023-04-16 02:11:15 -0500

    lenis scroll y intento de scroll snap

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <HeaderItem></HeaderItem>
+  <main id="smooth-wrapper">
+    <div id="smooth-content">
+      <HomePage></HomePage>
+    </div>
+  </main>
   <Footer-item></Footer-item>
   
   
 </template>
 
 <script>
+import Lenis from '@studio-freight/lenis'
 import HeaderItem from './components/HeaderItem.vue';
 import FooterItem from './components/FooterItem.vue';
+import HomePage from './pages/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
     HeaderItem,
-    FooterItem
+    FooterItem,
+    HomePage,
+  },
+  mounted() {
+    const lenis = new Lenis()
+    lenis.on('scroll', (e) => {
+      console.log(e)
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
   }
 }
 </script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,128 @@
 <template>
-  <div>
-    <h1>Bienvenido a mi aplicación</h1>
-    <p>Esta es la página principal</p>
+  <div class="container">
+    <h1 ref="titulo">Animación de texto</h1>
+
+      <div class="slider">
+          <img src="https://promart.vteximg.com.br/arquivos/ids/570404-1000-1000/22773.jpg?v=637401121588630000" />
+          <img src="https://promart.vteximg.com.br/arquivos/ids/6706390-1000-1000/22772.jpg?v=638102737892600000" />
+      </div>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
+        <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sed dolores officia incidunt dignissimos asperiores delectus sequi, veniam itaque fuga quaerat excepturi! Officia cum repellendus facere. Nulla veritatis inventore quibusdam.
+    </p>
   </div>
 </template>
 
+
+
 <script>
+import { gsap } from 'gsap';
+
 export default {
   data() {
     return {
@@ -13,6 +130,10 @@ export default {
     };
   },
   mounted() {
+    const titulo = this.$refs.titulo;
+    const animacion = gsap.timeline();
+
+    animacion.to(titulo, { duration: 1, perspective:100, text: "Hola mundo!" });
   },
   methods: {
   },
